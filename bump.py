172f8c76b1f724279ee133432acd1a454cb4cf9e
Denis Angell <dangell@transia.co>
2023-03-30 06:46:22 +0000

    lint

--- bump.py
+++ bump.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import json
@@ -8,12 +7,12 @@ from gen import get_definitions
 
 
 def read_file(path: str):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read()
 
 
 def write_file(path: str, data: str):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         return f.write(data)
 
 
@@ -23,7 +22,7 @@ def read_json(path: str):
 
 
 def write_json(path: str, data: Dict[str, Any]):
-    with open(path, 'w') as json_file:
+    with open(path, "w") as json_file:
         json.dump(data, json_file, indent=4)
 
 
@@ -34,21 +33,24 @@ def get_list(tk: str, data: Dict[str, Any]):
 
 
 IGNORE_LIST: List[str] = [
-    'Done',
-    'Unknown',
-    'Transaction',
-    'LedgerEntry',
-    'Validation',
-    'Metadata'
+    "Done",
+    "Unknown",
+    "Transaction",
+    "LedgerEntry",
+    "Validation",
+    "Metadata",
 ]
 
-is_debug: bool = bool(os.environ.get('IS_DEBUG')) or False
-is_reset: bool = bool(os.environ.get('IS_RESET')) or False
+is_release: bool = bool(os.environ.get("RELEASE")) or False
+is_debug: bool = bool(os.environ.get("DEBUG")) or False
+is_reset: bool = bool(os.environ.get("RESET")) or False
+
+
 def run(definitions: Dict[str, Any], name: str):
-    types: Dict[str, Any] = read_json('./map.json')['types']
-    type_map: Dict[str, Any] = read_json('./map.json')['type_map']
+    types: Dict[str, Any] = read_json("./map.json")["types"]
+    type_map: Dict[str, Any] = read_json("./map.json")["type_map"]
     new_type_map: Dict[str, Any] = {}
-    for tk, tv in definitions['TYPES'].items():
+    for tk, tv in definitions["TYPES"].items():
         if tk in IGNORE_LIST:
             continue
 
@@ -57,33 +59,43 @@ def run(definitions: Dict[str, Any], name: str):
             type_map[tk]
         except Exception as e:
             types[tk] = tv
-            if is_debug: print(f'TYPE ADDED: {e}')
-        
+            if is_debug:
+                print(f"TYPE ADDED: {e}")
+
         if tk not in [k for k, _ in type_map.items()]:
             continue
 
         # definitions to map
-        for f in definitions['FIELDS']:
+        for f in definitions["FIELDS"]:
             type: str = f[0]
-            nth: str = f[1]['nth']
+            nth: str = f[1]["nth"]
 
-            if tk != f[1]['type']:
+            if tk != f[1]["type"]:
                 continue
 
             new_type_map[tk][str(nth)] = {}
 
             if str(nth) not in get_list(tk, type_map):
-                new_type_map[tk][str(nth)] = f'|{nth}|{type}|{name}|n/a|'
+                new_type_map[tk][str(nth)] = f"|{nth}|{type}|{name}|n/a|"
             else:
-                old_type = type_map[tk][str(nth)].split('|')[2]
+                old_type = type_map[tk][str(nth)].split("|")[2]
                 if old_type != type:
-                    print(f'TYPES: Merge conflict for {type}. {old_type} already exists at {nth}.')
-                    response = input(f"Do you want to overwrite `{old_type}`? (yes/no) ").lower()
+                    print(
+                        f"TYPES: Merge conflict for {type}. {old_type} already exists at {nth}."
+                    )
+                    response = (
+                        input(
+                            f"Do you want to overwrite `{old_type}`? (yes/no) "
+                        ).lower()
+                        if not is_release
+                        else "no"
+                    )
                     if response == "yes":
-                        new_type_map[tk][f'{nth}'] = f'|{nth}|{type}|{name}|n/a|'
+                        new_type_map[tk][f"{nth}"] = f"|{nth}|{type}|{name}|n/a|"
                     else:
-                        raise KeyboardInterrupt(f'Please update `{old_type}` at {nth} and try again.')
-                    # new_type_map[tk][f'{nth}'] = f'|{nth}|{type}|{name}|n/a|'
+                        raise KeyboardInterrupt(
+                            f"Please update `{old_type}` at {nth} and try again."
+                        )
                 else:
                     new_type_map[tk][str(nth)] = type_map[tk][str(nth)]
 
@@ -91,46 +103,61 @@ def run(definitions: Dict[str, Any], name: str):
         if not is_reset:
             for k, _ in type_map[tk].items():
                 if str(k) not in get_list(tk, new_type_map):
-                    old_type = type_map[tk][str(k)].split('|')[2]
-                    old_amendment = type_map[tk][str(k)].split('|')[3]
-                    if is_debug: print(f'MISSING FIELD: {tk} - {old_type} - {k} - {old_amendment}')
+                    old_type = type_map[tk][str(k)].split("|")[2]
+                    old_amendment = type_map[tk][str(k)].split("|")[3]
+                    if is_debug:
+                        print(
+                            f"MISSING FIELD: {tk} - {old_type} - {k} - {old_amendment}"
+                        )
                     new_type_map[tk][str(k)] = type_map[tk][str(k)]
 
     if not is_reset:
         for k, _ in type_map.items():
             if str(k) not in new_type_map:
-                if is_debug: print(f'MISSING TYPE: {k}')
+                if is_debug:
+                    print(f"MISSING TYPE: {k}")
                 new_type_map[str(k)] = type_map[str(k)]
 
-    result_map: Dict[str, Any] = read_json('./map.json')['result_map']
+    result_map: Dict[str, Any] = read_json("./map.json")["result_map"]
     new_result_map: Dict[str, Any] = {}
-    for rk, rv in definitions['TRANSACTION_RESULTS'].items():
+    for rk, rv in definitions["TRANSACTION_RESULTS"].items():
         # TRANSACTION_RESULTS
         if str(rv) not in result_map:
-            new_result_map[str(rv)] = f'|{rv}|{rk}|{name}|n/a|'
+            new_result_map[str(rv)] = f"|{rv}|{rk}|{name}|n/a|"
         else:
-            old_result = result_map[str(rv)].split('|')[2]
+            old_result = result_map[str(rv)].split("|")[2]
             if old_result != rk:
-                    print(f'TRANSACTION_RESULTS: Merge conflict for {rk}. {old_result} already exists at {rv}.')
-                    response = input(f"Do you want to overwrite the `{old_result}` at {rv}? (yes/no) ").lower()
-                    if response == "yes":
-                        new_result_map[str(rv)] = f'|{rv}|{rk}|{name}|n/a|'
-                    else:
-                        raise KeyboardInterrupt(f'Please update `{old_result}` at {rv} and try again.')
-                # new_result_map[str(rv)] = f'|{rv}|{rk}|{name}|n/a|'
+                print(
+                    f"TRANSACTION_RESULTS: Merge conflict for {rk}. {old_result} already exists at {rv}."
+                )
+                response = (
+                    input(
+                        f"Do you want to overwrite the `{old_result}` at {rv}? (yes/no) "
+                    ).lower()
+                    if not is_release
+                    else "no"
+                )
+                if response == "yes":
+                    new_result_map[str(rv)] = f"|{rv}|{rk}|{name}|n/a|"
+                else:
+                    raise KeyboardInterrupt(
+                        f"Please update `{old_result}` at {rv} and try again."
+                    )
             else:
                 new_result_map[str(rv)] = result_map[str(rv)]
-    
+
     # map to map
     if not is_reset:
         for k, _ in result_map.items():
             if str(k) not in new_result_map:
-                old_result = result_map[str(k)].split('|')[2]
-                old_amendment = result_map[str(k)].split('|')[3]
-                if is_debug: print(f'MISSING TRANSACTION RESULT: {old_result} - {k} - {old_amendment}')
+                old_result = result_map[str(k)].split("|")[2]
+                old_amendment = result_map[str(k)].split("|")[3]
+                if is_debug:
+                    print(
+                        f"MISSING TRANSACTION RESULT: {old_result} - {k} - {old_amendment}"
+                    )
                 new_result_map[str(k)] = result_map[str(k)]
-    
-    output_value: str = ''
+
     output_value: str = """
 # SFCode Registry Tables
 
@@ -151,7 +178,7 @@ python3 bump.py | action | name | path
 
 If you would like to know what sfields your rippled build is missing then run with:
 
-- `IS_DEBUG=True python3 bump.py definitions Hooks ./rippled`
+- `DEBUG=True python3 bump.py definitions Hooks ./rippled`
 
 
 """
@@ -159,53 +186,59 @@ If you would like to know what sfields your rippled build is missing then run wi
         if len(v.items()) == 0:
             continue
         type_nth: str = types[k]
-        output_value += f'## {k.upper()}'
-        output_value += '\n'
-        output_value += f'Type {type_nth}'
-        output_value += '\n'
+        output_value += f"## {k.upper()}"
+        output_value += "\n"
+        output_value += f"Type {type_nth}"
+        output_value += "\n"
         output_value += """
 |Field Code|Field Name|Used by|Reserved by|
 |-|-|-|-|
 """
-        sorted_fields = {k: v for k, v in sorted(v.items(), key=lambda item: int(item[0]))}
+        sorted_fields = {
+            k: v for k, v in sorted(v.items(), key=lambda item: int(item[0]))
+        }
         for _, iv in sorted_fields.items():
             output_value += iv
-            output_value += '\n'
+            output_value += "\n"
             iv = None
 
-        output_value += '\n'
-        output_value += '\n'
+        output_value += "\n"
+        output_value += "\n"
 
         new_type_map[k] = sorted_fields
-    
-    output_value += f'## TRANSACTION RESULTS' + '\n'
-    output_value += '\n'
+
+    output_value += f"## TRANSACTION RESULTS" + "\n"
+    output_value += "\n"
     output_value += """
 |Response Code|Response Name|Used by|Reserved by|
 |-|-|-|-|
 """
-    sorted_results = {k: v for k, v in sorted(new_result_map.items(), key=lambda item: int(item[0]))}
+    sorted_results = {
+        k: v for k, v in sorted(new_result_map.items(), key=lambda item: int(item[0]))
+    }
     for k, v in sorted_results.items():
         output_value += v
-        output_value += '\n'
+        output_value += "\n"
         v = None
 
-    output_value += '\n'
-    output_value += '\n'
+    output_value += "\n"
+    output_value += "\n"
 
-    write_file('./README.md', output_value)
-    write_json('./map.json', {
-        "types": types,
-        "type_map": new_type_map,
-        "result_map": sorted_results,
-    })
+    write_file("./README.md", output_value)
+    write_json(
+        "./map.json",
+        {
+            "types": types,
+            "type_map": new_type_map,
+            "result_map": sorted_results,
+        },
+    )
 
-action_list: List[str] = ['definitions', 'rippled']
+
+action_list: List[str] = ["definitions", "rippled"]
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print(
-            "Usage: python3 bump.py <action> <amendment> <path>"
-        )
+        print("Usage: python3 bump.py <action> <amendment> <path>")
         sys.exit()
 
     action: str = sys.argv[1]
@@ -220,11 +253,11 @@ if __name__ == "__main__":
         sys.exit()
 
     try:
-        if action == 'definitions':
+        if action == "definitions":
             definitions: Dict[str, Any] = read_json(path)
             run(definitions, amendment)
-        if action == 'rippled':
-            r_path: str = path + '/src/ripple'
+        if action == "rippled":
+            r_path: str = path + "/src/ripple"
             definitions = get_definitions(r_path)
             run(definitions, amendment)
     except Exception as e:
